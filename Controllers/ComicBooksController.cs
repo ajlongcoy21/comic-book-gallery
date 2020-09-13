258fc634b506cb8ba6cf258c43d5af32c8e5d16e
ajlongcoy21 <ajlongcoy21@gmail.com>
2020-09-13 17:35:53 -0500

    udated detail action method to use actionresult types

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -13,9 +13,16 @@ namespace ComicBookGallery.Controllers
     {
         private readonly ILogger<ComicBooksController> _logger;
 
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic books controller";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return Redirect("/");
+            }
+
+            return Content("Hello from the comic books controller!");
+
+            
         }
 
         public ComicBooksController(ILogger<ComicBooksController> logger)
