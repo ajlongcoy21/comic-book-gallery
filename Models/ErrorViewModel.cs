00ee8bb686ad56f01a8a38e6b9640fe974681156
ajlongcoy21 <ajlongcoy21@gmail.com>
2020-09-13 17:05:26 -0500

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ComicBookGallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
