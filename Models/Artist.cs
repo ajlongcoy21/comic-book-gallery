b59abb54c60d1c46317cad36d14508a67a1f65cb
ajlongcoy21 <ajlongcoy21@gmail.com>
2020-09-14 21:37:23 -0500

    added models: artist and comicbook

---
+++ Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ComicBookGallery.Models
+{
+    public class Artist
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+
+        public Artist()
+        {
+        }
+    }
+}
---
+++ Models/ComicBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+        public ComicBook()
+        {
+        }
+    }
+}
