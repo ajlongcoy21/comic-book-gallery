f95a40a7276e325ae48e43aa3c4cfb655ea4f9ac
ajlongcoy21 <ajlongcoy21@gmail.com>
2020-09-20 16:10:27 -0500

    updated the default route

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace ComicBookGallery
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=ComicBooks}/{action=Index}/{id?}");
             });
         }
     }
